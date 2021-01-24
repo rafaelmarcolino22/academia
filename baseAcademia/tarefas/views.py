c08dd6540be0198a41acdd69bba4712f42cc0829
ozzy <ralfrr6@gmail.com>
2021-01-24 17:37:39 -0300

    content

--- baseAcademia/tarefas/views.py
+++ baseAcademia/tarefas/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 def Base(request):
-    return render(request, 'oi.html')
+    return render(request, 'base.html')
+
+def rotinaTreinos(request):
+    
+    return render(request, 'rotina.html')    
 
 # Create your views here.
